46e3aa150a4d42349a126fe163a26daaa6bf907b
Mayer Lench <mlench@finitydevelopment.com>
2022-12-12 09:14:10 -0500

    removes auth log

--- src/components/MainStack.tsx
+++ src/components/MainStack.tsx
@@ -14,7 +14,6 @@ const StackNavigator = stackNavigatorFactory();
 export const MainStack = () => {
 
     const auth = useAuth()
-    console.log('🚀 ~ file: MainStack.tsx:17 ~ MainStack ~ auth', auth)
     return (
         <BaseNavigationContainer>
             <StackNavigator.Navigator
